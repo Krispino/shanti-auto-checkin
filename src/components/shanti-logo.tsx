56da073517c2ce4f13ad2d2bde2fc9a720e0c6ee
krispino <crispimfabio@gmail.com>
2026-05-21 08:53:19 -0300

    feat: add ShantiLogo shared component

---
+++ src/components/shanti-logo.tsx
@@ -0,0 +1,11 @@
+export function ShantiLogo() {
+  return (
+    <div className="flex justify-center mb-8">
+      <img
+        src="/arquivos/logooficial1.JPG"
+        alt="Shanti Pousada"
+        style={{ height: 64, objectFit: "contain" }}
+      />
+    </div>
+  );
+}
